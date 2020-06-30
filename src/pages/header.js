8917311c009b1a7b0fc47c03687915f09aa4f3dc
ch04937 <97hernandez.c@gmail.com>
2020-06-30 17:49:11 -0500

    header: prep for accessToken

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { removeState } from "../utils/localStorage";
+
 import styles from "../stylesheets/hf.module.scss";
 
 export default function Header() {
+	const token = localStorage.getItem("accessToken");
+
+	function signOut() {
+		removeState();
+	}
 	return (
 		<div className={styles.header}>
-			<Link to="/">Ark Planner</Link>
+			<div>
+				<Link to="/">Ark Planner</Link>
+			</div>
+			{!token ? (
+				<div>
+					<Link to="/login">Sign In</Link>
+				</div>
+			) : (
+				<div className={styles.nav}>
+					<div>
+						<Link to="/user">Profile</Link>
+					</div>
+					<div onClick={signOut}>Sign Out</div>
+				</div>
+			)}
 		</div>
 	);
 }
--- src/pages/main.js
+++ src/pages/main.js
@@ -2,15 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import styles from "../stylesheets/main.module.scss";
-import app from "../stylesheets/App.module.scss";
 
 export default function Main() {
 	return (
-		<div className={styles.main}>
-			<div className={app.btn}>
-				<Link to="/login">Login</Link>
-				<Link to="/register">Register</Link>
-			</div>
+		<div className={styles.home}>
+			<Link to="/login">Login</Link>
+			<Link to="/register">Register</Link>
 		</div>
 	);
 }
