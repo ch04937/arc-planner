accd2a0bb9489c794af0fc834b53e208f43ea479
ch04937 <97hernandez.c@gmail.com>
2020-07-09 18:20:11 -0500

    style: governor profile

---
+++ src/components/profileContent.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import { Icon } from "semantic-ui-react";
+
+import { AuthContext } from "../utils/context/Auth/AuthState";
+import { validateField, validateNumber } from "../utils/validateAuth";
+
+import form from "../stylesheets/App.module.scss";
+
+export default function ProfileContent() {
+	const { userProfile } = useContext(AuthContext);
+	function addGovernorName(name) {
+		console.log("name", name);
+	}
+	return (
+		<div>
+			{" "}
+			<div className={styles.card}>
+				{userProfile && userProfile.governorName ? (
+					<>
+						<lavel>Governor name:</lavel>
+						<p>{userProfile.governorName}</p>
+						<Icon name="pencil" />
+					</>
+				) : (
+					<Formik
+						initialValues={{
+							governorName: "",
+							cityHall: "",
+							castleLevel: "",
+						}}
+						onSubmit={(values, actions) => {
+							addGovernorName(values);
+						}}
+					>
+						{({ errors, touched, validateForm }) => (
+							<Form className={form.form}>
+								<label>Governor name:</label>
+								{errors.governorName &&
+									touched.governorName && (
+										<div className={styles.validate}>
+											{errors.governorName}
+										</div>
+									)}
+								<Field
+									type="text"
+									name="governorName"
+									validate={validateField}
+								/>
+
+								<label>City Hall Level:</label>
+								{errors.cityHall && touched.cityHall && (
+									<div className={styles.validate}>
+										{errors.cityHall}
+									</div>
+								)}
+								<Field
+									type="text"
+									name="cityHall"
+									validate={validateNumber}
+								/>
+								<label>Castle Level:</label>
+								{errors.castleLevel && touched.castleLevel && (
+									<div className={styles.validate}>
+										{errors.castleLevel}
+									</div>
+								)}
+								<Field
+									type="text"
+									name="castleLevel"
+									validate={validateNumber}
+								/>
+								<button
+									type="submit"
+									onClick={() => validateForm()}
+								>
+									Submit
+								</button>
+							</Form>
+						)}
+					</Formik>
+				)}
+			</div>
+			<div className={styles.card}>
+				{userProfile && userProfile.email}
+			</div>
+			<div className={styles.card}>
+				{userProfile && userProfile.email}
+			</div>
+			<div className={styles.card}>
+				{userProfile && userProfile.email}
+			</div>
+			<div className={styles.card}>
+				{userProfile && userProfile.email}
+			</div>
+		</div>
+	);
+}
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,102 +1,35 @@
 import React, { useContext } from "react";
-import { Formik, Form, Field } from "formik";
-import { Icon } from "semantic-ui-react";
-
-import { AuthContext } from "../utils/context/Auth/AuthState";
-import { validateField, validateNumber } from "../utils/validateAuth";
 
 import styles from "../stylesheets/profile.module.scss";
-import form from "../stylesheets/App.module.scss";
 
 export default function Profile() {
-	const { userProfile } = useContext(AuthContext);
-	function addGovernorName(name) {
-		console.log("name", name);
-	}
 	return (
 		<div className={styles.wrapper}>
-			<div>
-				<h1>User Profile</h1>
+			<div className={styles.title}>
+				<h1>Governor Profile</h1>
 			</div>
-			<div className={styles.cards}>
-				<div className={styles.card}>
-					{userProfile && userProfile.governorName ? (
-						<>
-							<lavel>Governor name:</lavel>
-							<p>{userProfile.governorName}</p>
-							<Icon name="pencil" />
-						</>
-					) : (
-						<Formik
-							initialValues={{
-								governorName: "",
-								cityHall: "",
-								castleLevel: "",
-							}}
-							onSubmit={(values, actions) => {
-								addGovernorName(values);
-							}}
-						>
-							{({ errors, touched, validateForm }) => (
-								<Form className={form.form}>
-									<label>Governor name:</label>
-									{errors.governorName &&
-										touched.governorName && (
-											<div className={styles.validate}>
-												{errors.governorName}
-											</div>
-										)}
-									<Field
-										type="text"
-										name="governorName"
-										validate={validateField}
-									/>
-
-									<label>City Hall Level:</label>
-									{errors.cityHall && touched.cityHall && (
-										<div className={styles.validate}>
-											{errors.cityHall}
-										</div>
-									)}
-									<Field
-										type="text"
-										name="cityHall"
-										validate={validateNumber}
-									/>
-									<label>Castle Level:</label>
-									{errors.castleLevel &&
-										touched.castleLevel && (
-											<div className={styles.validate}>
-												{errors.castleLevel}
-											</div>
-										)}
-									<Field
-										type="text"
-										name="castleLevel"
-										validate={validateNumber}
-									/>
-									<button
-										type="submit"
-										onClick={() => validateForm()}
-									>
-										Submit
-									</button>
-								</Form>
-							)}
-						</Formik>
-					)}
+			<div className={styles.profile}>
+				<div className={styles.banner}>
+					<p>SOMETHING HERE</p>
+					<p>SOMETHING HERE</p>
+					<p>SOMETHING HERE</p>
+					<p>SOMETHING HERE</p>
 				</div>
-				<div className={styles.card}>
-					{userProfile && userProfile.email}
+				<div className={styles.content}>
+					<p>CONTENT</p>
+					<p>CONTENT</p>
+					<p>CONTENT</p>
+					<p>CONTENT</p>
+					<p>CONTENT</p>
 				</div>
-				<div className={styles.card}>
-					{userProfile && userProfile.email}
-				</div>
-				<div className={styles.card}>
-					{userProfile && userProfile.email}
-				</div>
-				<div className={styles.card}>
-					{userProfile && userProfile.email}
+			</div>
+			<div className={styles.buttons}>
+				<div className={styles.btns}>
+					<p>button</p>
+					<p>button</p>
+					<p>button</p>
+					<p>button</p>
+					<p>button</p>
 				</div>
 			</div>
 		</div>
