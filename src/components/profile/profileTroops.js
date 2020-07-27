0f45277d88a75a6d108eccb74350b9e53398b9e7
ch04937 <97hernandez.c@gmail.com>
2020-07-27 17:58:44 -0500

    feat: onclick add troops count

--- src/components/profile/profileTroops.js
+++ src/components/profile/profileTroops.js
@@ -1,53 +1,69 @@
 import React, { useContext } from "react";
 
+import TroopBtns from "./troopBtns";
+
 import { ArkContext } from "../../utils/context/Ark/ArkState";
 
 import styles from "../../stylesheets/profile.module.scss";
-import TroopBtns from "./troopBtns";
 
 export default function ProfileTroops() {
 	const { profile } = useContext(ArkContext);
 	return (
-		<>
-			<div className={styles.troops}>
-				<div className={styles.tier}>
-					<p>TIER 3 ARCHER:</p>
-					{profile && profile.t3arch}
-					<TroopBtns />
-				</div>
-				<div className={styles.tier}>
-					<p>TIER 3 INFANTRIES:</p>
-					{profile && profile.t3inf}
-				</div>
-				<div className={styles.tier}>
-					<p>TIER 3 CAVALRY:</p>
-					{profile && profile.t3cav}
-				</div>
-				<div className={styles.tier}>
-					<p>TIER 4 ARCHER:</p>
-					{profile && profile.t4arch}
-				</div>
-				<div className={styles.tier}>
-					<p>TIER 4 INFANTRIES:</p>
-					{profile && profile.t4inf}
-				</div>
-				<div className={styles.tier}>
-					<p>TIER 4 CAVALRY:</p>
-					{profile && profile.t4cav}
-				</div>
-				<div className={styles.tier}>
-					<p>TIER 5 ARCHER:</p>
-					{profile && profile.t5arch}
-				</div>
-				<div className={styles.tier}>
-					<p>TIER 5 INFANTRIES:</p>
-					{profile && profile.t5inf}
-				</div>
-				<div className={styles.tier}>
-					<p>TIER 5 CAVALRY:</p>
-					{profile && profile.t5cav}
+		<div className={styles.t_wrapper}>
+			<div className={styles.t_troops}>
+				<div className={styles.t_content}>TIER 3 ARCHER:</div>
+				<div className={styles.t_content}>
+					<TroopBtns type="t3arch" count={profile.t3arch} />
 				</div>
 			</div>
-		</>
+			<div className={styles.t_troops}>
+				<div className={styles.t_content}>TIER 3 INFANTRIES:</div>
+				<div className={styles.t_content}>
+					<TroopBtns type="t3inf" count={profile.t3inf} />
+				</div>
+			</div>
+			<div className={styles.t_troops}>
+				<div className={styles.t_content}>TIER 3 CAVALRY:</div>
+				<div className={styles.t_content}>
+					<TroopBtns type="t3cav" count={profile.t3cav} />
+				</div>
+			</div>
+			<div className={styles.t_troops}>
+				<div className={styles.t_content}>TIER 4 ARCHER:</div>
+				<div className={styles.t_content}>
+					<TroopBtns type="t4arch" count={profile.t4arch} />
+				</div>
+			</div>
+			<div className={styles.t_troops}>
+				<div className={styles.t_content}>TIER 4 INFANTRIES:</div>
+				<div className={styles.t_content}>
+					<TroopBtns type="t4arch" count={profile.t4arch} />
+				</div>
+			</div>
+			<div className={styles.t_troops}>
+				<div className={styles.t_content}>TIER 4 CAVALRY:</div>
+				<div className={styles.t_content}>
+					<TroopBtns type="t4arch" count={profile.t4arch} />
+				</div>
+			</div>
+			<div className={styles.t_troops}>
+				<div className={styles.t_content}>TIER 5 ARCHER:</div>
+				<div className={styles.t_content}>
+					<TroopBtns type="t4arch" count={profile.t4arch} />
+				</div>
+			</div>
+			<div className={styles.t_troops}>
+				<div className={styles.t_content}>TIER 5 INFANTRIES:</div>
+				<div className={styles.t_content}>
+					<TroopBtns type="t4arch" count={profile.t4arch} />
+				</div>
+			</div>
+			<div className={styles.t_troops}>
+				<div className={styles.t_content}>TIER 5 CAVALRY:</div>
+				<div className={styles.t_content}>
+					<TroopBtns type="t4arch" count={profile.t4arch} />
+				</div>
+			</div>
+		</div>
 	);
 }
--- src/components/profile/troopBtns.js
+++ src/components/profile/troopBtns.js
@@ -1,11 +1,71 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 
-export default function TroopBtns() {
+import { ArkContext } from "../../utils/context/Ark/ArkState";
+
+import custom from "../../stylesheets/custom-styles.module.scss";
+import styles from "../../stylesheets/profile.module.scss";
+
+export default function TroopBtns({ type, count }) {
+	const [isPositive, setIsPositive] = useState(true);
+	const [hasChanged, setHasChanged] = useState(false);
+
+	let addedTroops = 0;
+
+	const { profile } = useContext(ArkContext);
+
+	function togglePositive() {
+		setIsPositive(!isPositive);
+	}
+	function saveTroops(params) {
+		setHasChanged(false);
+		console.log("type", type);
+		console.log("params", params);
+	}
+
+	function updateCount(count) {
+		setHasChanged(true);
+		addedTroops += count;
+		console.log("addedTroops", addedTroops);
+	}
 	return (
-		<div>
-			<p>+100</p>
-			<p>+500</p>
-			<p>+1000</p>
+		<div className={custom.center}>
+			<div className={styles.t_content}>
+				{count}
+				{hasChanged ? (
+					<>{isPositive ? " + " + addedTroops : "-"}</>
+				) : (
+					""
+				)}
+			</div>
+			<div
+				onClick={togglePositive}
+				className={isPositive ? custom.green_btn : custom.red_btn}
+			>
+				{isPositive ? "+" : "-"}
+			</div>
+			<div className={custom.sets}>
+				<div
+					className={isPositive ? custom.green_btn : custom.red_btn}
+					onClick={() => updateCount(100)}
+				>
+					100
+				</div>
+				<div
+					className={isPositive ? custom.green_btn : custom.red_btn}
+					onClick={() => updateCount(500)}
+				>
+					500
+				</div>
+				<div
+					className={isPositive ? custom.green_btn : custom.red_btn}
+					onClick={() => updateCount(1000)}
+				>
+					1000
+				</div>
+			</div>
+			<button className={custom.save_btn} onClick={() => saveTroops()}>
+				Save
+			</button>
 		</div>
 	);
 }
